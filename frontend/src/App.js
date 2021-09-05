0beba73d344e98b922ad455542682af951f24f71
Pathong <paopeaw8246@gmail.com>
2021-09-05 12:36:19 +0700

    test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,19 +4,35 @@ import Template from './components/template';
 import Nav from './components/nav'
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Home from './components/Home'
-import Webcam_block from './components/Webcam'
+// import Webcam_block from './components/Webcam'
+
+import {createStore} from 'redux';
+import { Provider } from 'react-redux'
+import allReducers from './Redux/Reducer/Allreducer';
+// import {ToggleCamReducer} from './Reducer/ShowCam';
+
+const store = createStore(
+  allReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
+  )
+
+
+
+
 function App() {  
   return (
-    <Router>
-      <div className="App">
-        <Nav/>
-
-        <Switch>
-          <Route path="/" exact component= {Home}/>
-          <Route path="/user/:firstName" component={Template}  />
-        </Switch>
-      </div>
+    <Provider store = {store}>
+      <Router>
+        <div className="App">
+          <Nav/>    
+          <Switch>
+            <Route path="/" exact component={Home}/>
+            <Route path="/user/:firstName" component={Template}  />
+          </Switch>
+        </div>
     </Router>
+    </Provider>
+    
   );
 }
 export default App;
